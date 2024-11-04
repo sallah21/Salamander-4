b33d9363a1d04a45e14776efc8b87070122425e0
Dawid  Salamon <luckyluj24@gmail.com>
2024-11-04 11:27:11 +0100

    More advanced instruction decoding

--- rtl/ID.sv
+++ rtl/ID.sv
@@ -2,10 +2,14 @@
     Instruction decoder module
 */
 
+`include "OP_CODES.sv"
+
 module ID (
     input [5:0] INSTR,
+    input       ID_CE,
     output [2:0] OP_CODE,
     output [1:0] ADDR, 
+    output [3:0] CE,
     output      ACC_CE,
 );
 /*
@@ -21,9 +25,38 @@ module ID (
  * 
  */
 
+wire [3:0] CE_w;
 
-assign ADDR = INSTR[1:0];
-assign OP_CODE = INSTR [4:2];
-assign ACC_CE = INSTR [5];
+ always_comb begin
+    case (INSTR [4:2])
+    OP_ST: begin 
+        case(INSTR[1:0]) 
+            2'b00: begin
+                CE_w <= 4'b0001;
+            end 
+            2'b01: begin
+                CE_w<= 4'b0010;
+            end 
+            2'b10: begin
+                CE_w <= 4'b0100;
+            end 
+            2'b11: begin
+                CE_w<= 4'b1000;
+            end 
+            default: begin
+                CE_w<= 4'b0000;
+            end 
+        endcase 
+    end 
+    default: begin
+        CE_w<= 4'b0000;
+    end 
+    endcase 
+ end 
+
+assign CE = (ID_CE) ? CE_w : 4'b0000;
+assign ADDR = (ID_CE) ? INSTR[1:0] : 2'bzz;
+assign OP_CODE = (ID_CE) ? INSTR [4:2] : 3'bzzz;
+assign ACC_CE = (ID_CE)? INSTR [5] : 1'bz;
     
 endmodule
