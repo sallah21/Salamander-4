ef286a8e0330886632dcd071a1054999b87b1c3c
Dawid  Salamon <luckyluj24@gmail.com>
2024-11-04 11:14:33 +0100

    ALU carry in added

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -13,29 +13,29 @@ module ALU #(
     input [2:0] OP_CODE, // 3-bit code for operation
     input [SIZE-1:0] left_operand,
     input [SIZE-1:0] right_operand, 
-    output logic carry_out,
-    output logic [SIZE-1:0] op_out
+    input carry_in,
+    output carry_out,
+    output [SIZE-1:0] op_out
 );
 
     logic carry_out_w;
     logic [SIZE-1:0] op_out_w;
 
-    // TODO: add ci (Carry in)
     always_comb begin
         carry_out_w = 0;
         op_out_w = '0;
 
         if (CE) begin
             case (OP_CODE)
-                OP_ADD: {carry_out_w, op_out_w} = left_operand + right_operand;
-                OP_SUB: {carry_out_w, op_out_w} = left_operand - right_operand;
+                OP_ADD: {carry_out_w, op_out_w} = left_operand + right_operand + carry_in;
+                OP_SUB: {carry_out_w, op_out_w} = left_operand - right_operand + carry_in;
                 OP_AND: op_out_w = left_operand & right_operand;
                 OP_OR:  op_out_w = left_operand | right_operand;
                 OP_XOR: op_out_w = left_operand ^ right_operand;
                 OP_NOT: op_out_w = ~left_operand;
                 OP_LD:  op_out_w = right_operand;
                 OP_ST:  op_out_w = left_operand;
-                default: op_out_w = '0; // Default to zero for undefined opcodes
+                default: op_out_w = 'z; 
             endcase
         end
     end
