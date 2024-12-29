1c946f6edb1bdb8c9a8f07e19c8a52a2f7df2fc5
Dawid  Salamon <luckyluj24@gmail.com>
2024-12-29 18:24:59 +0100

    OP_CODES comments added

--- rtl/OP_CODES.sv
+++ rtl/OP_CODES.sv
@@ -2,13 +2,14 @@
     Operational codes for Arithmetical Logical Unit (ALU)
 */
 
+// Operation codes for ALU in 3-bit format
 typedef enum logic [2:0]{  
-    OP_ADD  = 3'b000,
-    OP_SUB  = 3'b001,
-    OP_AND  = 3'b010,
-    OP_OR   = 3'b011,
-    OP_XOR  = 3'b100,
-    OP_NOT  = 3'b101,
-    OP_LD   = 3'b110,
-    OP_ST   = 3'b111
+    OP_ADD  = 3'b000, // Add operation
+    OP_SUB  = 3'b001, // Subtract operation
+    OP_AND  = 3'b010, // AND operation
+    OP_OR   = 3'b011, // OR operation
+    OP_XOR  = 3'b100, // XOR operation
+    OP_NOT  = 3'b101, // NOT operation
+    OP_LD   = 3'b110, // Load operation
+    OP_ST   = 3'b111  // Store operation
 } OPERATION_CODE;
