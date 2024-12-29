0eee6bfdb27717911e19ae6e801bce1f731d1e39
Dawid  Salamon <luckyluj24@gmail.com>
2024-12-29 18:27:30 +0100

    Program counter wrapping added and formal verification

--- rtl/PC.sv
+++ rtl/PC.sv
@@ -4,8 +4,8 @@
 */
 
 module PC
-   #(
-     parameter SIZE = 5 // Corrected parameter declaration
+  #(
+     parameter SIZE = 5
    )
    (
      input clk,
@@ -18,7 +18,7 @@ module PC
   reg [SIZE-1:0] cnt_r;
   reg max_size_reached_r;
   localparam SIZEMINONE = SIZE - 1;
-  localparam [SIZE-1:0] MAX_VAL = (2**SIZE) -1; 
+  localparam [SIZE-1:0] MAX_VAL = (2**SIZE) -1;
 
   always @(posedge clk or negedge rstn)
   begin
@@ -31,12 +31,15 @@ module PC
     begin
       if (inc)
       begin
-        if (cnt_r <= MAX_VAL) begin
-            cnt_r <= cnt_r + 5'b1; 
+        if (cnt_r < MAX_VAL)
+        begin
+          cnt_r <= cnt_r + 5'b1;
         end
-        else begin
-            max_size_reached_r <= 1'b1;
-         end 
+        else
+        begin
+          cnt_r <= {SIZE{1'b0}};
+          max_size_reached_r <= 1'b1;
+        end
 
       end
     end
@@ -44,4 +47,35 @@ module PC
 
   assign cnt_val = cnt_r;
   assign max_size_reached = max_size_reached_r;
+
+  `ifdef FORMAL
+  //////////////////////////////////////////////////////////////////////////////////////////
+  ///////////// Assertions
+  //////////////////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+  // Asstertion 1: Counter value should not exceed MAX_VAL
+  //////////////////////////////////////////////////////////////////////////////////////////
+
+  property cnt_val_does_not_exceed_max_val;
+    @ (posedge clk or negedge rstn)
+      $stable(cnt_val <= MAX_VAL);
+  endproperty
+
+  assert cnt_val_does_not_exceed_max_val else
+           $display("Counter value exceeded MAX_VAL at %0t ns", $time);
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////
+  // Asstertion 2: Max size reached asserted correctly
+  //////////////////////////////////////////////////////////////////////////////////////////
+  property max_size_reached_asserted_correctly;
+    @ (posedge clk or negedge rstn)
+      $stable(max_size_reached == (cnt_val == MAX_VAL));
+  endproperty
+
+  assert max_size_reached_asserted_correctly else
+           $display("Max size reached not asserted correctly at %0t ns", $time);
+
+  `endif // FORMAL
 endmodule
