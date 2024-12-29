b277f7f5e7f5d03abe609fd9650a86b05f39a715
Dawid  Salamon <luckyluj24@gmail.com>
2024-12-29 18:26:32 +0100

    Addded write to program memory

--- rtl/PROG_MEM.sv
+++ rtl/PROG_MEM.sv
@@ -9,20 +9,32 @@ module PROG_MEM #(
   ) (
     input clk,
     input rstn,
+    input W,
+    input  [DATA_SIZE-1:0] DATA_WR,
     input  [ADDR_SIZE-1:0] ADDR,
     output [DATA_SIZE-1:0] DATA
   );
 
   logic [DATA_SIZE-1:0] int_mem_r [2**ADDR_SIZE-1:0];
+
+  logic [DATA_SIZE-1:0] DATA_reg;
   always @(posedge clk or negedge rstn)
   begin
     if (!rstn)
     begin
       for (int i = 0; i < 2**ADDR_SIZE; i++)
       begin
-        int_mem_r[i] <= '0;
+        int_mem_r[i] <= {DATA_SIZE{1'b0}};
       end
     end
+    else if (W)
+    begin
+      int_mem_r [ADDR] <= DATA_WR; // Write operation
+    end
+    else
+    begin
+      DATA_reg <= int_mem_r [ADDR]; // Read operation
+    end
   end
-  assign DATA = int_mem_r [ADDR];
+  assign DATA = DATA_reg;
 endmodule
