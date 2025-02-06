eb1d8a374c268bcd4077937a0d2b453845ab0f3e
sallah21 <luckyluj24@gmail.com>
2025-02-06 16:58:17 +0100

    added output for toplevel to preserve logic and some preverve for registers

--- rtl/STACK.sv
+++ rtl/STACK.sv
@@ -5,7 +5,7 @@
 
 module STACK #(
     parameter DATA_SIZE = 4,
-    parameter STACK_SIZE = 5
+    parameter STACK_SIZE = 4
 )
 (
     input CLK,
@@ -21,51 +21,35 @@ module STACK #(
 localparam STACK_MEM_SIZE = 2**STACK_SIZE;
 
 reg [DATA_SIZE-1:0] DATA_RD_r;
-reg [STACK_SIZE-1:0] STACK_ADDR_r;
-reg [DATA_SIZE-1:0] mem_r [STACK_MEM_SIZE-1:0];
-reg stack_full_r;
-reg stack_empty_r;
+reg [STACK_SIZE-1:0] STACK_ADDR_r ;
+reg [DATA_SIZE-1:0] mem_r [0:STACK_MEM_SIZE-1] /* synthesis preserve */;
+reg stack_full_r /* synthesis preserve */;
+reg stack_empty_r /* synthesis preserve */;
 
 always @(posedge CLK or negedge RSTN)
 begin
     if (!RSTN)
     begin
-        DATA_RD_r <= 0;
-        STACK_ADDR_r <= 0;
+        DATA_RD_r <= '0;
+        STACK_ADDR_r <= '0;
         stack_full_r <= 1'b0;
-        for (int i = 0; i < STACK_MEM_SIZE; i++)
-        begin
-            mem_r[i] <= 'x;
-        end
+        stack_empty_r <= 1'b1;  // Stack is empty on reset
     end      
     else
     begin
-        if (W)
+        if (W && !stack_full_r)  // Only write if not full
         begin
-            if (STACK_ADDR_r == STACK_MEM_SIZE-1)
-            begin
-                stack_full_r <= 1'b1;
-            end 
-            else 
-            begin
-                stack_full_r <= 1'b0;
-                mem_r[STACK_ADDR_r] <= DATA_WR;
-                STACK_ADDR_r <= STACK_ADDR_r + 1;
-            end
-        end
-        else if (R)
-        begin
-            if (STACK_ADDR_r == 0) 
-            begin 
-                DATA_RD_r <= 'x;
-                stack_empty_r <= 1'b1;
-            end        
-            else 
-            begin
+            mem_r[STACK_ADDR_r] <= DATA_WR;
+            STACK_ADDR_r <= STACK_ADDR_r + 1;
             stack_empty_r <= 1'b0;
+            stack_full_r <= (STACK_ADDR_r == STACK_MEM_SIZE-2);  // Will be full after this write
+        end
+        else if (R && !stack_empty_r)  // Only read if not empty
+        begin
             DATA_RD_r <= mem_r[STACK_ADDR_r-1];
             STACK_ADDR_r <= STACK_ADDR_r - 1;
-            end
+            stack_full_r <= 1'b0;
+            stack_empty_r <= (STACK_ADDR_r == 1);  // Will be empty after this read
         end
     end 
 end
